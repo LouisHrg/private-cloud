96e224c479c7e708cbf6ded0f2c746310d63b82c
Louis Harang <dev@narah.io>
2023-05-10 00:32:37 +0200

    feat: update

--- scripts/passbolt/init-admin-user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $DIR/../../.env
-
-echo "### Creating first admin user"
-docker exec passbolt su -m -c "/var/www/passbolt/bin/cake passbolt register_user -u ${EMAIL} -f ${PASSBOLT_USER} -l ${PASSBOLT_USER} -r admin" -s /bin/sh www-data
